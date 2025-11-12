5149ab37394b0312b0b63fa374d036d2be04f086
Miguel Fox <wdfoxy34@gmail.com>
2025-11-12 14:38:54 -0300

    choer(models0: creat Admin class

---
+++ Api_navarro/Api_navarro/models/Admin.cs
@@ -0,0 +1,30 @@
+﻿namespace Api_navarro.models
+{
+    public class Admin: user
+    {
+        public Admin(string name, string email, int cpf, double numero, string senha) : base(name, email, cpf, numero, senha)
+        {
+        }
+
+        public override string NomeBase(string nome)
+        {
+            return base.NomeBase(nome);
+        }
+        public override string EmailBase(string email)
+        {
+            return base.EmailBase(email);
+        }
+        public override string SenhaBase(string senha)
+        {
+            return base.SenhaBase(senha);
+        }
+        public override int CPFBase(int cpf)
+        {
+            return base.CPFBase(cpf);
+        }
+        public override double NumeroBase(double numero)
+        {
+            return base.NumeroBase(numero);
+        }
+    }
+}
