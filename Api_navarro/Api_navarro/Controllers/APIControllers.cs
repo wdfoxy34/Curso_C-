19835c4ceffd886ab3cd067cd707bb3b16711c70
Miguel Fox <wdfoxy34@gmail.com>
2025-11-13 11:41:04 -0300

    feat(cursos, vendasApi): creat duo feats web site

---
+++ Api_navarro/Api_navarro/Controllers/APIControllers.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api_navarro.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class APIControllers : Controller
+    {
+        static List<Curso> List =
+        [
+            new Curso(1, "eneias", "presidente","yes", 250),
+            new Curso(2, "Matheus", "ADS","Null", 23),
+        ];
+
+        [HttpGet]
+        public IActionResult get()
+        {
+            return Ok(List);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult getbyId(int id) 
+        {
+            var curso = List.Find(x => x.Id == id);
+            if (curso == null) 
+            {
+                return NotFound( new { Mensage = $"Curso ao encontrado id={id}"});
+            }
+            return Ok(curso);
+        }
+
+
+
+    }
+}
---
+++ Api_navarro/Api_navarro/Curso.cs
@@ -0,0 +1,21 @@
+﻿namespace Api_navarro
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public decimal Preco {  get; set; }
+
+        public Curso(int id, string name, string description, string type, decimal preco)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Type = type;
+            Preco = preco;
+        }
+    }
+
+}
