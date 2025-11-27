be1930f363e230547f43dcab0152b3696d67ffc4
Miguel Fox <wdfoxy34@gmail.com>
2025-11-27 11:43:16 -0300

    feat(Infrastructure/Repositories): adds all user-related repositories

---
+++ Api_navarro/Api_navarro/Infrastructure/Repositories/AdminRepository.cs
@@ -0,0 +1,43 @@
+﻿using Api_navarro.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Api_navarro.Domain.Interfaces;
+using Api_navarro.Domain.Entities;
+using System.ComponentModel;
+
+namespace Api_navarro.Infrastructure.Repositories
+{
+    public class AdminRepository: IAdminRepository
+    {
+        private readonly Datacontext _context;
+        public AdminRepository(Datacontext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<Admin?>> GetByNameAsync(string name)
+        {
+            return await _context.Admins.Where(u => u.Name == name).ToListAsync();
+        }
+
+        public async Task<Admin?> GetByEmailAsync(string email)
+        {
+            return await _context.Admins.FirstOrDefaultAsync(u => u.Email == email);
+        }
+        public async Task<Admin?> GetByCPFAsync(string cpf)
+        {
+            return await _context.Admins.FirstOrDefaultAsync(u => u.CPF == cpf);
+        }
+
+        public async Task<Admin?> GetByTelefoneAsync(string telefone)
+        {
+            return await _context.Admins.FirstOrDefaultAsync(u => u.Telefone == telefone);
+        }
+
+        public async Task AddAsync(Admin admin)
+        {
+            await _context.Admins.AddAsync(admin);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ Api_navarro/Api_navarro/Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,44 @@
+﻿using Api_navarro.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Api_navarro.Domain.Interfaces;
+using Api_navarro.Domain.Entities;
+using System.ComponentModel;
+
+namespace Api_navarro.Infrastructure.Repositories
+{
+    public class ClientRepository: IClientRepository
+    {
+        private readonly Datacontext _context;
+
+        public ClientRepository(Datacontext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<Client>> GetByNameAsync(string name)
+        {
+            return await _context.Clients.Where(u => u.Name == name).ToListAsync();
+        }
+
+        public async Task<Client?> GetByEmailAsync(string email)
+        {
+            return await _context.Clients.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<Client?> GetByCPFAsync(string cpf)
+        {
+            return await _context.Clients.FirstOrDefaultAsync(u => u.CPF == cpf);
+        }
+
+        public async Task<Client?> GetByTelefoneAsync(string telefone)
+        {
+            return await _context.Clients.FirstOrDefaultAsync(u => u.Telefone == telefone);
+        }
+
+        public async Task AddAsync(Client client)
+        {
+            await _context.Clients.AddAsync(client);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
