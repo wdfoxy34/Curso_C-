d6c6d01363fb19fda3187159e1c574918b71e331
Miguel Fox <wdfoxy34@gmail.com>
2025-11-27 11:25:28 -0300

    feat(Datacontext): adds all remaining classes to datacontext

--- Api_navarro/Api_navarro/Infrastructure/Persistence/Datacontext.cs
+++ Api_navarro/Api_navarro/Infrastructure/Persistence/Datacontext.cs
@@ -1,14 +1,20 @@
-﻿using Api_navarro.Domain.models;
+﻿using Api_navarro.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Api_navarro.Data
-{
-    public class Datacontext : DbContext
-    {
-        public Datacontext(DbContextOptions<Datacontext> options) : base(options)
-        {
-        }
+namespace Api_navarro.Infrastructure.Persistance;
 
-        public DbSet<Curso> Cursos { get; set; }
+public class Datacontext : DbContext
+{
+    public Datacontext(DbContextOptions<Datacontext> options) : base(options)
+    {
     }
+
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Client> Clients { get; set; }
+
+    public DbSet<Curso> Cursos { get; set; }
+    public DbSet<Pagamento> Pagamentos { get; set; }
+    public DbSet<Turma> Turmas { get; set; }
+    public DbSet<User> Users { get; set; }
+
 }
