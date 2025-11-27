65966c2b75a42bf7fdc4b0f89b7b944144fc1980
Miguel Fox <wdfoxy34@gmail.com>
2025-11-27 11:42:25 -0300

    feat(Domain/Interfaces): adds all user-related interfaces

---
+++ Api_navarro/Api_navarro/Domain/Interfaces/IAdminRepository.cs
@@ -0,0 +1,12 @@
+﻿using Api_navarro.Domain.Entities;
+namespace Api_navarro.Domain.Interfaces
+{
+    public interface IAdminRepository
+    {
+        Task<List<Admin?>> GetByNameAsync(string name);
+        Task<Admin?> GetByEmailAsync(string email);
+        Task<Admin> GetByCPFAsync(string email);
+        Task<Admin> GetByTelefoneAsync(string telefone);
+        Task AddAsync(Admin admin);
+    }
+}
---
+++ Api_navarro/Api_navarro/Domain/Interfaces/IClientRepository.cs
@@ -0,0 +1,12 @@
+﻿using Api_navarro.Domain.Entities;
+namespace Api_navarro.Domain.Interfaces
+{
+    public interface IClientRepository
+    {
+        Task<List<Client?>> GetByNameAsync(string name);
+        Task<Client?> GetByEmailAsync(string email);
+        Task<Client?> GetByCPFAsync(string email);
+        Task<Client?> GetByTelefoneAsync(string email);
+        Task AddAsync(Client user);
+    }
+}
