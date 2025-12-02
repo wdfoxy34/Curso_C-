3d7b634493a03c0295d8c40082b6e6fd7d6239fc
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-12-01 21:45:52 -0300

    chore(CursoRepositorie): Upddates both interface and repo

---
+++ Api_navarro/Api_navarro/Domain/Interfaces/ICursoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Api_navarro.Domain.Entities;
+namespace Api_navarro.Domain.Interfaces
+{
+    public interface ICursoRepository
+    {
+        Task<List<Curso?>> GetByNameAsync(string name);
+        Task<Curso?> GetByIdAsync(int id);     
+        Task<List<Curso?>> GetByTypeAsync(string type);
+        Task<List<Curso?>> GetByPriceAsync(decimal price, string filter);
+        Task<List<Curso?>> GetByCompletionAsync(bool status);
+        Task AddAsync(Curso curso);
+       
+    }
+}
