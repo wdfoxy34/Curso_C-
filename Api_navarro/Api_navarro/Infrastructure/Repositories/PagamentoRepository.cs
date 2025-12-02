63af9bff11916cfaab47c5b036dae44dc01fec43
Gabriel Fernando <gabrielfanselmo08@gmail.com>
2025-12-02 15:57:11 -0300

    Implementa PagamentoUsecase, ajusta entidade Pagamento e prepara estrutura para PagamentoRepository

---
+++ Api_navarro/Api_navarro/Infrastructure/Repositories/PagamentoRepository.cs
@@ -0,0 +1,33 @@
+﻿using Api_navarro.Domain.Entities;
+using Api_navarro.Domain.Interfaces;
+using Api_navarro.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Api_navarro.Infrastructure.Repositories
+{
+    public class PagamentoRepository : IPagamentoRepository
+    {
+        private readonly Datacontext _context;
+
+        public PagamentoRepository(Datacontext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Pagamento pagamento)
+        {
+            _context.Pagamentos.Add(pagamento);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Pagamento?> GetbyIdAsync(int id)
+        {
+            return await _context.Pagamentos.Include(p => p.Curso).Include(p => p.Pagador)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+        public async Task<Pagamento?> GetbyPaymentAsync(bool payment)
+        {
+            return await _context.Pagamentos.Include(p => p.Curso).Include(p => p.Pagador)
+                .FirstOrDefaultAsync(p => p.Payment == payment);
+        }
+    }
+}
